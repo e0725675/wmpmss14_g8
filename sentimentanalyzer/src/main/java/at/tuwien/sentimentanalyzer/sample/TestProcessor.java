d92091aedbc6fc15e3ebb6de557143d070a10f15
e0725675 <etnemelc.clemente@gmail.com>
2014-05-14 17:58:25 +0200

    twitter fix

--- sentimentanalyzer/src/main/java/at/tuwien/sentimentanalyzer/sample/TestProcessor.java
+++ sentimentanalyzer/src/main/java/at/tuwien/sentimentanalyzer/sample/TestProcessor.java
@@ -2,8 +2,10 @@ package at.tuwien.sentimentanalyzer.sample;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.log4j.Logger;
 
 public class TestProcessor implements Processor{
+	public static Logger log = Logger.getLogger(TestProcessor.class);
 	public void process(Exchange exchange) throws Exception {
 	    System.out.println("Hello!");
 	  }
