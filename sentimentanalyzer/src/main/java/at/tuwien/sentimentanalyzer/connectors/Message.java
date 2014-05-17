7ee2343702f875029204beb730ff2d797e5ae65b
e0725675 <etnemelc.clemente@gmail.com>
2014-05-17 12:45:56 +0200

    added Message object

---
+++ sentimentanalyzer/src/main/java/at/tuwien/sentimentanalyzer/connectors/Message.java
@@ -0,0 +1,62 @@
+package at.tuwien.sentimentanalyzer.connectors;
+
+import static javax.persistence.GenerationType.AUTO;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * All messages, tweets, posts, whatever, will be aggregated to this format
+ * @author CLF
+ *
+ */
+public class Message {
+	
+	/**
+	 * The content of the message (e.g. a tweet, a fecebook post, ...)
+	 */
+	private String message;
+	/**
+	 * The author of the message
+	 */
+	private String author;
+	/**
+	 * When was the message posted
+	 */
+	private Date timePosted;
+	/**
+	 * From which source did the message come from? (twitter, facebook, reddit, ...)
+	 * Since i do not know which sources we will have i leave this as String
+	 */
+	private String source;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getTimePosted() {
+		return timePosted;
+	}
+	public void setTimePosted(Date timePosted) {
+		this.timePosted = timePosted;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+}
---
+++ sentimentanalyzer/src/main/java/at/tuwien/sentimentanalyzer/connectors/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author CLF
+ *
+ */
+package at.tuwien.sentimentanalyzer.connectors;
