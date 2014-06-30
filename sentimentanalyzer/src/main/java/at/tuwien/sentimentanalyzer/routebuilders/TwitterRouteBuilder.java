03c1b529dbac19956b43489da8005a4209f877ca
fimich <fimichsim@gmail.com>
2014-06-30 16:51:12 +0200

    Configuration File

--- sentimentanalyzer/src/main/java/at/tuwien/sentimentanalyzer/routebuilders/TwitterRouteBuilder.java
+++ sentimentanalyzer/src/main/java/at/tuwien/sentimentanalyzer/routebuilders/TwitterRouteBuilder.java
@@ -10,9 +10,9 @@ public class TwitterRouteBuilder extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		//from("twitter://search?type=polling&numberOfPages=1&delay=60&keywords={{search.term}}&consumerKey=hEnR5kZzziT64SMb1IZjmJw9f&consumerSecret=LZc6bLhihxVCicn9JBi28EHC73C6u1llQ2PjA9W6uEUastACrv&accessToken=2493103232-0ath7kWee8wC98OryubP1TvLa0SmDQM3sfm8jlU&accessTokenSecret=m4PwikkabfFBgagebNZW1h8qYG25cks8K6uyNvOX89AAE").
+		//from("{{twitter.path}}").
 		//to("bean:messageConverter?method=statusToMessage").
-		from("timer:messageockerTimer?period=5000").
+		from("timer:messageockerTimer?period={{twitter.period}}").
 			routeId("messageMockerGenerationRoute").
 		beanRef("messageMocker", "nextMessage").
 		to("direct:incomingMessages");
